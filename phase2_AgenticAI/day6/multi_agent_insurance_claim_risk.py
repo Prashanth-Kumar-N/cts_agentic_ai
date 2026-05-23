3a615cebf81ba1cb4a904b602d5a1283eb49fab2
sriramancr <81683167+sriramancr@users.noreply.github.com>
2026-05-23 13:46:12 +0530

    Add files via upload

---
+++ phase2_AgenticAI/day6/multi_agent_insurance_claim_risk.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+'''
+An Intelligent Insurance Claim Risk Assessment System uses a multi-agent architecture to evaluate insurance claims and identify potential fraud risks.
+
+-------------------------------
+What is Loss Amount calculation
+-------------------------------
+** The Loss Amount Calculation estimates how much money the insurance company is likely to pay for a claim
+   after considering damage estimates, repair costs, and policy deductions.
+
+Agent 1
+--------
+Performs financial and risk-related calculations such as estimated loss amount and claim risk scoring using structured
+claim data.
+
+------------------------------
+What is Risk Score calculation
+------------------------------
+** The Risk Score Calculation determines how risky or potentially fraudulent an insurance claim is based on
+   multiple behavioral, financial, and historical factors.
+   “How suspicious or risky is this claim?”
+
+Agent 2
+-------
+Performs reasoning on these calculated outputs to recommend actions like approval, manual review, escalation, or fraud investigation.
+
+This approach helps insurance companies automate claim assessment, improve accuracy, and reduce fraudulent payouts.
+'''
+
+
